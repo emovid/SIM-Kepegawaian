e048cbed1173ecd51560ac0db76a934312b45c8c
emovid <misbach.imaduddin@yahoo.com>
2016-01-25 16:44:16 +0700

    Finalisasi Antarmuka Dasar

---
+++ fix_ui/resources/views/home.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('head.style')
+<link rel="stylesheet" href="css/bootstrap-select.css">
+@show
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    
+                     <form role="form" id="form1" method="post" action="demo_merge.php" class="col-md-5 well">
+                      <label class="control-label" for="lunch">Jenis Surat:</label>
+                    <div class="form-group">
+                        
+                      <select id="lunch" name="tpl" class="form-control selectpicker" data-live-search="true" title="Pilih Jenis Surat" >
+                       <option value="demo1.docx">Cuti Besar</option>
+                        <option value="demo2.docx">Cuti Haji Umroh</option>
+                        <option value="demo3.docx">Cuti Tahunan Penting</option>
+                        <option value="demo_ms_word.docx">Ijin Belajar</option>
+                        <option value="5">Ijin Cuti Bersalin Naban</option>
+                        <option value="6">Ijin Cuti Bersalin</option>
+                        <option value="7">Ijin Cuti Sakit Walikota</option>
+                        <option value="8">Ijin Cuti Sakit</option>
+                        <option value="9">Keuangan Bulanan</option>
+                        <option value="10">Koperasi Lina</option>
+                        <option value="11">Pengajuan Taspen</option>
+                        <option value="12">Pensiun Janda</option>
+                        <option value="13">Pensiun, Sakit, Walikota</option>
+                        <option value="14">Permohonan Ijin Belajar</option>
+                        <option value="15">Permohonan Pensiun</option>
+                        <option value="16">PLT(Penunjukkan)</option>
+                        <option value="17">Rekomendasi KA Dinas</option>
+                        <option value="18">Seleksi Masuk Perguruan Tinggi</option>
+                        <option value="19">SPMJ(Surat Pernyataan Telah Menduduki Jabatan</option>
+                        <option value="20">SPMT(Surat Pernyataan Melakukan Tugas</option>
+                        <option value="21">SPPD(Surat Perintah Perjalanan Dinas)</option>
+                        <option value="22">Edaran</option>
+                        <option value="23">Pemberitahuan</option>
+                        <option value="24">Tugas 1</option>
+                        <option value="25">Tugas 2</option>
+                        <option value="26">Undangan</option>
+                      </select>
+                    </div>
+
+
+                <div class="form-group">
+                 <label class="control-label" for="NIP">Masukkan NIP:</label>
+                      <input name="NIP" id="NIP" type="text" class="form-control"/>
+                </div>
+
+
+
+
+                      <td>&nbsp;</td>
+                        <input type="submit" name="btn_result" value="Merge" class="btn btn-primary" />
+                        <!--        
+                        <input type="submit" name="btn_template" value="See template" />
+                        <input type="submit" name="btn_script" value="See PHP script" /> 
+                        -->
+
+
+
+                  </form>
+                    <div class="col-xs-7">
+                        <h4>Keterangan</h4>
+                        <ol>
+                            <li>Pilih jenis surat yang akan dibuat</li>
+                            <li>Pilih jenis surat yang akan dibuat</li>
+                        </ol>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>                            
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('body.script')
+<script src="js/bootstrap-select.js"></script>
+<script>
+$(document).ready(function() {
+    $('.selectpicker').selectpicker({
+        style: 'btn-info',
+        size: 4
+    });
+    
+})
+</script>
+@show
---
+++ fix_ui/resources/views/layouts/app.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SIM-Kepegawaian Dinas Pendidikan DIY</title>
+  <meta name="author" content="Vinsensius Satya, Yosef Brian, Misbach Imaduddin">
+
+  <link rel="shortcut icon" href="img/favicon.ico">
+
+  <!-- Fonts -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+  <!-- Styles -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/get-shit-done.css" rel="stylesheet" />  
+  <link href="css/demo.css" rel="stylesheet">
+  @yield('head.style')
+
+  @yield('head.script')
+  <style>
+        body {
+            font-family: 'Lato';
+            background-color: aliceblue;
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+  <nav class="navbar navbar-default">
+      <div class="container">
+          <div class="navbar-header">
+
+              <!-- Collapsed Hamburger -->
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                  <span class="sr-only">Toggle Navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+
+              <!-- Branding Image -->
+              <a class="navbar-brand" href="{{ url('/') }}">
+                  Manajemen Surat
+              </a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="app-navbar-collapse">
+              <!-- Left Side Of Navbar -->
+              <ul class="nav navbar-nav">
+                  <li><a href="{{ url('/home') }}">Home</a></li>
+              </ul>
+
+              <!-- Right Side Of Navbar -->
+              <ul class="nav navbar-nav navbar-right">
+                  <!-- Authentication Links -->
+                  @if (Auth::guest())
+                      <li><a href="{{ url('/login') }}">Login</a></li>
+                      <li><a href="{{ url('/register') }}">Register</a></li>
+                  @else
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                              {{ Auth::user()->name }} <span class="caret"></span>
+                          </a>
+
+                          <ul class="dropdown-menu" role="menu">
+                              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                          </ul>
+                      </li>
+                  @endif
+              </ul>
+          </div>
+      </div>
+  </nav>
+
+  @yield('content')
+
+  <!-- JavaScripts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  @yield('body.script')
+</body>
+
+<script type="text/javascript">
+  // enable the option for savegin as a file, the PHP script will test if it is running on localhost anyway.
+  //if (window.location.hostname=='localhost') document.getElementById('save_as_file').style.display='table-row';
+</script>
+</body>
+</html>
