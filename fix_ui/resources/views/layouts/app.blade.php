b1520998d1e709ca48f48d069ef337c1bd33be78
emovid <misbach.imaduddin@yahoo.com>
2016-01-29 17:12:16 +0700

    update lokasi icon

--- fix_ui/resources/views/layouts/app.blade.php
+++ fix_ui/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <title>SIM-Kepegawaian Dinas Pendidikan DIY</title>
   <meta name="author" content="Vinsensius Satya, Yosef Brian, Misbach Imaduddin">
 
-  <link rel="shortcut icon" href="img/favicon.ico">
+  <link rel="shortcut icon" href="{{ URL::asset('img/favicon.ico') }}">
 
   <!-- Fonts -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
