521b901a2f1410ee1680e72f5ad50526099d66be
emovid <misbach.imaduddin@yahoo.com>
2016-01-29 15:17:23 +0700

    mengubah routing sehingga langsung menuju laman login

--- fix_ui/app/Http/routes.php
+++ fix_ui/app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +33,8 @@ Route::group(['middleware' => ['web']], function () {
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
+    Route::get('/', 'HomeController@index');  
+  
     Route::get('/home', 'HomeController@index');
   
     Route::get('/getData', 'HomeController@getData');
